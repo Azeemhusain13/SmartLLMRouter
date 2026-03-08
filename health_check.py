8f65c32f8868b4634a7f674c17f1ffd1bfefc746
Azeem <110116509+Azeemhusain13@users.noreply.github.com>
2026-03-08 15:23:52 +0530

    Update health_check.py

--- health_check.py
+++ health_check.py
@@ -1,10 +1,11 @@
 import time
 from openai import OpenAI
 import os
+import streamlit as st
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key=os.getenv("OPENROUTER_API_KEY"),
+    api_key=st.secrets["OPENROUTER_API_KEY"]
 )
 
 
